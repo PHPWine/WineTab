fa6880ea58bc7d3e6180a65a8ca479520882c3f6
nielsoffice <nieldigitalsolution@gmail.com>
2023-12-05 20:58:43 +0800

    prerelease for v2.2.0

--- src/WineTab.php
+++ src/WineTab.php
@@ -45,41 +45,52 @@ error_reporting(E_ALL);
 */
 
 class WineTab 
-extends \PHPWineOptimizedHtml\Doctrine\Tab 
-implements \PHPWineOptimizedHtml\Interfaces\TabInterface
-{
-    public function __construct(array $wine = [])
-    {
-        parent::__construct($wine);
-        if (
-            !wine_compare(
-                $this->icon,
-                "\x43\x4f\115\x50\117\x4e\x45\116\124\x5f\x52\105\107\111\123\x54\x45\x52\x45\x44\x5f\113\x45\x59\137\x41\103\x43\117\122\x44\x49\117\116"
-            )
-        ) {
-            throw new \Exception(
-                "\141\164\164\145\x6d\160\x74\x20\x69\156\166\141\154\x69\144\x20\x6b\x65\x79\x20\x3a\x20" .
-                    $this->icon .
-                    "\x21"
-            );
-            die();
+extends \PHPWineOptimizedHtml\Doctrine\Tab  
+implements \PHPWineOptimizedHtml\Interfaces\TabInterface 
+{ 
+    public function __construct(array $wine = []) 
+    { 
+        parent::__construct($wine); 
+        if ( 
+            !wine_compare( 
+                $this->icon, 
+                "COMPONENT_REGISTERED_KEY_COMPONENT" 
+            ) 
+        ) { 
+            throw new \Exception( 
+                "attempt invalid key : " . 
+                    $this->icon . 
+                    "!" 
+            ); 
+            die(); 
+        } 
+        if ( 
+            !wine_compare( 
+                $this->prefix, 
+                "COMPONENT_REGISTERED_KEY_PREFIX" 
+            ) 
+        ) { 
+            throw new \Exception( 
+                "attempt invalid key : " . 
+                    $this->prefix . 
+                    "!" 
+            ); 
+            die(); 
         }
-        if (
+        if ( 
             !wine_compare(
-                $this->prefix,
-                "\103\x4f\x4d\120\x4f\x4e\x45\116\x54\137\122\105\x47\111\x53\x54\x45\x52\x45\104\137\x4b\105\x59\x5f\x50\x52\105\x46\111\x58"
-            )
-        ) {
-            throw new \Exception(
-                "\141\164\164\x65\155\x70\164\40\151\156\x76\141\154\151\x64\40\x6b\145\171\x20\72\x20" .
-                    $this->prefix .
-                    "\41"
-            );
-            die();
-        }
-    }
-    public function tab()
-    {
-        return $this->wine_action();
-    }
-}
+         
+               $this->hook,
+             
+              'DOCTRINE_REGISTERED_KEY_HOOKS') 
+           ) {
+           throw new \Exception("attempt invalid key : ".$this->hook."!");
+           die();
+       }
+
+    } 
+    public function tab() 
+    { 
+        return $this->wine_action(); 
+    } 
+} 
